715d4cb9d3d51bc8b8d1a025e11b5abcb9f391d7
muhammadjonr <mrahmonaliev22032007@gmail.com>
2024-03-07 14:59:22 +0500

    full change

--- src/context.jsx
+++ src/context.jsx
@@ -1,41 +1,64 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import reducer from "./reducer";
 import data from "./data";
 
 const AppContext = createContext();
 
 const initialState = {
-  loading: false,
-  cart: data,
-  total: 0,
-  amount: 20,
-};
+    loading: false,
+    cart: data,
+    total: 0,
+    amount: 0,
+  }
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const clearCart = () => {
-    dispatch({ type: "CLEAR" });
-  };
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  const inc = (id) => {
-    dispatch({ type: "inc", payload: id });
-  };
+    const clearCart = () => {
+        dispatch({type: "CLEAR"})
+    }
 
-  return (
-    <AppContext.Provider
-      value={{
-        ...state,
-        clearCart,
-        inc,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
-};
+    const inc = (id) => {
+        dispatch({type: "inc", payload: id})
+    }
+    const dec = (id) => {
+        dispatch({type: "dec", payload: id})
+    }
+    const remove = (id) => {
+        dispatch({type: "remove", payload: id})
+    }
+
+    // const ctotal = () => {
+    //     dispatch({ type: "total" });
+    // }
+    // const price = () => {
+    //     dispatch({ type: "tprice" });
+    // }
+
+
+    useEffect(() => {
+        // ctotal() 
+        // price()
+        dispatch({type: "get_total"})
+    },[state])
+    
+
+
+    return (
+        <AppContext.Provider value={{
+            ...state,
+            clearCart,
+            inc,
+            dec,
+            remove,
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
 
 export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
+    return useContext(AppContext);
+} 
 
 export { AppContext, AppProvider };
